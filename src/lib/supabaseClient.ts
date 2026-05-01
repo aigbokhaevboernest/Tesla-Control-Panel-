1e61a0dd5f2dd9e0ba7e19e43a082511340b0edd
jameshilterson <jameshilterson@gmail.com>
2026-05-02 00:45:55 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/integrations/supabase/types";
 
 const SUPABASE_URL = "https://vvohhdltxfengpcpbxyh.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2b2hoZGx0eGZlbmdwY3BieHloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NTg4NDQsImV4cCI6MjA5MzEzNDg0NH0.I-_0INVANTg196qhXltkXnWglVeEBDbrHUzvxf2xD8I"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2b2hoZGx0eGZlbmdwY3BieHloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NTg4NDQsImV4cCI6MjA5MzEzNDg0NH0.I-_0INVANTg196qhXltkXnWglVeEBDbrHUzvxf2xD8I"
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
