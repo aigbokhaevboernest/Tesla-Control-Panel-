d5269dfa4d0d9e7599b652228b86d204b2da0bf9
jameshilterson <jameshilterson@gmail.com>
2026-05-03 22:25:59 +0100

    Update AdminLogin.tsx

--- src/admin/pages/AdminLogin.tsx
+++ src/admin/pages/AdminLogin.tsx
@@ -15,7 +15,7 @@ export default function AdminLogin() {
 
   const checkAdminRole = async (userId: string) => {
     const { data, error } = await supabase
-      .from("profile")
+      .from("profiles")
       .select("role")
       .eq("user_id", userId)
       .maybeSingle();
